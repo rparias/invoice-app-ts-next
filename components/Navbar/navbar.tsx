2de5fb359da690a9e91571a8124a676f8beae0d4
Ronald Arias <ariasron@hotmail.com>
2023-04-04 16:49:07 -0500

    adds spaces for logo and icons in navbar

--- components/Navbar/navbar.tsx
+++ components/Navbar/navbar.tsx
@@ -1,5 +1,10 @@
 import './navbar.scss';
 
 export default function Navbar() {
-	return <nav className="navbar">Im a nav</nav>;
+	return (
+		<nav className="navbar">
+			<div className="navbar__logo-container">logo</div>
+			<div className="navbar__icons-container">icons</div>
+		</nav>
+	);
 }
