4540c84bc2b05d802bb2b19537c84f429d3800d7
Ronald Arias <ariasron@hotmail.com>
2023-04-11 15:49:49 -0500

    adds navbar to layout and show on each page except design system

--- components/Navbar/navbar.tsx
+++ components/Navbar/navbar.tsx
@@ -1,9 +1,20 @@
+import Image from 'next/image';
+
 import './navbar.scss';
 
 export default function Navbar() {
 	return (
 		<nav className="navbar">
-			<div className="navbar__logo-container">logo</div>
+			<div className="navbar__logo-container">
+				<Image
+					className="logo"
+					src="/assets/logo.svg"
+					alt="App Logo"
+					width={40}
+					height={38}
+					priority
+				/>
+			</div>
 			<div className="navbar__icons-container">icons</div>
 		</nav>
 	);
